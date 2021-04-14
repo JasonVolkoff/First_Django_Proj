277bd0d14c4d023742cb89e70a8833e1f6915b15
JasonVolkoff <kashijason@gmail.com>
2021-04-14 07:48:50 -0700

    Added test paths within app_one urls and corresponding methods within views.

--- first_project/app_one/views.py
+++ first_project/app_one/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+
+def create(request):
+    return HttpResponse("Idk what to do here")
+
+
+def show(request, number):
+    return HttpResponse("placeholder to display blog number: {number}")
+
+
+def edit(request, number):
+    return HttpResponse("placeholder to edit blog {number}")
+
+
+def destroy(request, number):
+    return HttpResponse("Idk what to do here either")
